7e9f1bc2e5ba507c5cf1a1b5a35ac26c01acc830
LemonYellowParrot <ljy012900@gmail.com>
2020-11-20 03:53:51 +0900

    testfile

---
+++ src/main/java/controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package controller;
+
+import java.util.List;
+import model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import repository.EmployeeRepository;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    // get all employees
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+}
